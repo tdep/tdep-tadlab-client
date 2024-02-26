172a447395e5efb81d50e6457af4310bdd41a826
tdep <t.depewmusic@gmail.com>
2024-02-26 11:21:25 -0500

    adds portfolio components and global styling

--- app/portfolio/components/contact/contact.tsx
+++ app/portfolio/components/contact/contact.tsx
@@ -1,7 +1,23 @@
 export default function Contact() {
     return (
-        <>
-            <h1>This be my contact section</h1>
-        </>
+        <article id={"contact"} className={"contact-section"}>
+            <section className={"container"}>
+                <h2>Contact</h2>
+                <p>This is where all of my contact info will live!</p>
+                <ol>
+                    <li>Contact 1</li>
+                    <li>Contact 2</li>
+                    <li>Contact 3</li>
+                </ol>
+            </section>
+            <footer id={"other-info"}>
+                <div className={"container"}>
+                    <h3>Other Projects</h3>
+                    <a href={""} className={"link"}>Project 1</a>
+                    <a href={""} className={"link"}>Project 2</a>
+                    <a href={""} className={"link"}>Project 3</a>
+                </div>
+            </footer>
+        </article>
     )
 }
--- app/portfolio/components/portfolio/portfolio.tsx
+++ app/portfolio/components/portfolio/portfolio.tsx
@@ -1,7 +1,14 @@
 export default function Portfolio() {
     return (
         <>
-            <h1>This be my portfolio section</h1>
+            <article id={"portfolio"} className={"portfolio-section"}>
+                <section className={"container"}>
+                    <h2>Portfolion</h2>
+                    <p>This is where I will show screen captures of my work with links to other pages that will be functional demos</p>
+                    <h3>Description</h3>
+                    <p>This will be where I describe stuff.</p>
+                </section>
+            </article>
         </>
     )
 }
