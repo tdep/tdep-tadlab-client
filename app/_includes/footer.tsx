d9c2ff70e2a611058b6cfd8ac1b01205a04071fd
tdep <t.depewmusic@gmail.com>
2024-02-27 15:33:25 -0500

    adds fun emoji to footer

--- app/_includes/footer.tsx
+++ app/_includes/footer.tsx
@@ -1,8 +1,25 @@
+import {useEffect, useState} from "react";
+
 export default function Footer() {
+
+    const emojis = ["😂", "😍", "✨", "🌈", "🍙", "🍜", "🐻"];
+    const [madeWith, setMadeWith] = useState<number>(1);
+
+    const getRandomInteger = (min:number, max:number) => {
+        const minCeil = Math.ceil(min);
+        const maxFloor = Math.floor(max)
+        return Math.floor(Math.random() * (maxFloor - minCeil) + minCeil);
+    }
+
+    useEffect(() => {
+        setMadeWith(getRandomInteger(0, emojis.length))
+    }, []);
+
+
     return (
         <footer className={"page-footer"}>
             <div className={"container"}>
-                <p>Some footer stuff goes here</p>
+                <p>Created with {emojis[madeWith]} by Trevor DePew</p>
                 <ul>
                     <li><a href={""}>A link maybe?</a></li>
                     <li><a href={""}>Maybe another link?</a></li>
