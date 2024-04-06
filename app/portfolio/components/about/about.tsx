0f6f74f9041947d21fd0114a15244e1acea06180
tdep <t.depewmusic@gmail.com>
2024-04-06 16:56:51 -0400

    sets profile image css, removes aside temporarily

--- app/portfolio/components/about/about.tsx
+++ app/portfolio/components/about/about.tsx
@@ -2,21 +2,34 @@ export default function About() {
     return (
         <article id={"about"}>
             <section className={"container"}>
-                <h1>Trevor DePew</h1>
-                <h3>Software Engineer</h3>
+                <div className={"name-title"}>
+                    <h1>Trevor DePew</h1>
+                    <h3>Software Engineer</h3>
+                </div>
             </section>
-            <section className={"container"}>
-                <p>
-                    I am a <strong>Full Stack Software Engineer</strong> with a passion for solving problems about human
-                    behavior and its relationship with the natural and urban environments from infrastructure to climate
-                    and points between.<br/><br/>
-                    I have a core belief that truth and honesty with ourselves and each other is the best way to create
-                    an environment filled with meaningful and productive relationships.<br/><br/>
-                    Apart from my academic pursuits in classical composition and performance, I have worked as a
-                    technical support agent, piano technician, theater technician, and farmer.<br/><br/>
-                    My previous career pursuits have led me through a healthy number of varied experiences which have in
-                    turn given me a well-rounded set of skills that I use to broaden my perspective as a software engineer.
-                </p>
+            <section className={"container blurb"}>
+                <div id={"text-container"}>
+                    <p>
+                        I am a <strong>Full Stack Software Engineer</strong> with a passion for solving problems
+                        about human
+                        behavior and its relationship with the natural and urban environments from infrastructure to
+                        climate
+                        and points between.<br/><br/>
+                        I have a core belief that truth and honesty with ourselves and each other is the best way to
+                        create
+                        an environment filled with meaningful and productive relationships.<br/><br/>
+                        Apart from my academic pursuits in classical composition and performance, I have worked as a
+                        technical support agent, piano technician, theater technician, and farmer.<br/><br/>
+                        My previous career pursuits have led me through a healthy number of varied experiences which
+                        have in
+                        turn given me a well-rounded set of skills that I use to broaden my perspective as a
+                        software
+                        engineer.
+                    </p>
+                </div>
+                <div className={"photo-container"}>
+                    <img id={"photo"} src={""} alt={"Photo of Trevor DePew"}/>
+                </div>
             </section>
         </article>
     )
