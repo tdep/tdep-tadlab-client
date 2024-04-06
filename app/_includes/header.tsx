b2ed04555301d0632a3090e5a51be56f6a13eee8
tdep <t.depewmusic@gmail.com>
2024-04-06 16:17:26 -0400

    fixes styling for nav bar

--- app/_includes/header.tsx
+++ app/_includes/header.tsx
@@ -4,11 +4,17 @@ export default function Header() {
     return (
         <header>
             <div className={"container"}>
-                <h1>Trevor DePew</h1>
-                <h1>Portfolio</h1>
-                <Link className={"link-social"} href={"https://www.linkedin.com/in/trevor-depew/"} target={"_blank"}>LinkedIn</Link>
-                <Link className={"link-social"} href={"https://github.com/tdep"} target={"_blank"}>Github</Link>
-                <Link className={"link-social"} href={"https://tadepewmusic.com/"} target={"_blank"}>TAD</Link>
+                <div className={"name"}>
+                    <h1>Trevor DePew</h1>
+                </div>
+                <div className={"title"}>
+                    <h1>Portfolio</h1>
+                </div>
+                <ul>
+                    <li><Link className={"link-social"} href={"https://www.linkedin.com/in/trevor-depew/"} target={"_blank"}>LinkedIn</Link></li>
+                    <li><Link className={"link-social"} href={"https://github.com/tdep"} target={"_blank"}>Github</Link></li>
+                    <li><Link className={"link-social"} href={"https://tadepewmusic.com/"} target={"_blank"}>TAD</Link></li>
+                </ul>
             </div>
         </header>
     )
