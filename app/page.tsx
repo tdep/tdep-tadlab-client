5328e4b69292dd7381ae99dc3bd5c1d51a94e5d5
tdep <t.depewmusic@gmail.com>
2024-04-07 16:44:52 -0400

    adds api waking method to keep api running

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import styles from "./page.module.css";
 import Link from "next/link";
+import {TADLAB_API_URL} from "@/app/portfolio/lib/constants";
 
 export default function Home() {
+
+    const http = require('http');
+
+    setInterval(() => {
+        http.get(`${TADLAB_API_URL}`);
+    }, 25 * 60 * 1000);
+
   return (
     <main className={styles.main}>
       <h1>Yar, thar be dragons here.</h1>
