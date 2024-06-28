2eac9dc5ed6f74f5b96606b8f3cb6da33353b41e
tdep <t.depewmusic@gmail.com>
2024-06-28 19:03:03 -0400

    setting hover functionality

--- app/components/homeNav.tsx
+++ app/components/homeNav.tsx
@@ -1,25 +1,38 @@
-import Link from "next/link";
-
+'use client'
 export default function HomeNav() {
+
+    function handleHover() {
+        let chevron = document.getElementById("portfolio-chevron")
+        if (chevron) {
+            chevron.id = "on"
+        }
+    }
     return (
         <section>
             <nav className={"home-nav"}>
                 <ul id={"home-nav-ul"}>
-                    <li><div className={"home-link"}>
-                            <p>Visit my Portfolio to learn more about me.</p>
-                            <a href={"/portfolio"} className={"button"}>Portfolio</a>
+                    <li>
+                        <div className={"home-link"}>
+                            <p>Visit my portfolio to learn more about me
+                                <span id={"portfolio-chevron"}>&gt;</span>
+                            </p>
+                            <a href={"/portfolio"} onMouseOver={handleHover} className={"button"}>Portfolio</a>
                         </div>
                     </li>
                     <li>
                         <div className={"home-link"}>
-                            <p>Or check out the Blog to see what I've been up to lately.</p>
+                            <p>Or, check out the blog to see what I've been up to lately
+                                <span id={"blog-chevron"}>&gt;</span>
+                            </p>
                             <a href={""} className={"button"}>Blog</a>
                         </div>
 
                     </li>
                     <li>
                         <div className={"home-link"}>
-                            <p>Find out what the TadLab is all about here.</p>
+                            <p>Find out what the TadLab is all about here
+                                <span id={"tadlab-chevron"}>&gt;</span>
+                            </p>
                             <a href={""} className={"button"}>TadLab</a>
                         </div>
                     </li>
